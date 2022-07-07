0da620b232a3ac22acd0046a0417741ecafe6e8e
Alexis Javier Levano Saire <levanosaire@gmail.com>
2022-07-07 09:27:22 -0500

    asdad

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const navToggle = document.querySelector(".nav-toggle")
-const navMenu = document.querySelector(".nav-menu")
-navToggle.addEventListener("click", ()=>{
-	navMenu.classList("nav-menu_visible")
- })
+const form = document.getElementById('form')
+
+form.addEventListener('submit', function(e){
+	e.preventDefault()
+	const email = 'levanosaire@gmail.com'
+	const URL_BASE = `https://formsubmit.co/ajax/${email}`
+
+	const input = e.currentTarget.elements
+	const dataForm = {
+		name : input.name.value,
+		email : input.email.value,
+		message : input.message.value
+	}
+
+	window.fetch(URL_BASE, {
+    method: "POST",
+    headers: { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify(dataForm)
+})
+    alert('Tu mensaje se envio correctamente')
+    e.currentTarget.reset()
+
+} )
